113fd7785f88f617341d38b1b7752dac3e9d0124
zk-1 <zoe@starshade.ca>
2025-01-28 11:12:19 -0500

    add asset compiling for bootstrap

---
+++ app/assets/config/manifest.js
@@ -0,0 +1,7 @@
+//= link_tree ../images
+//= link_directory ../stylesheets .css
+//= link_directory ../../javascript .js
+//= link_tree ../../../vendor/javascript .js
+//= link controllers/application.js
+//= link controllers/hello_controller.js
+//= link controllers/index.js
